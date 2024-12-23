c377bb08564f0184c7c0ad86779081268cea7913
Chadderbox <chadderboxdev@gmail.com>
2024-12-23 22:43:21 +0000

    Rename Plugin to MoreSpeedsHost

---
+++ MoreSpeedsHost.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System.IO;
+using UnityEngine;
+
+namespace MoreSpeeds;
+
+/// <summary>
+/// Basic plugin host for the MoreSpeeds plugin.
+/// </summary>
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class MoreSpeedsHost : BaseUnityPlugin
+{
+    internal static ManualLogSource sLogger;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity Function")]
+    private void Awake()
+    {
+        var file = new FileInfo(typeof(MoreSpeedsHost).Assembly.Location);
+        
+        sLogger = base.Logger;
+        sLogger.LogInfo($"Loaded {MyPluginInfo.PLUGIN_GUID}::{file.CreationTime}.");
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity Function")]
+    private void Update()
+    {
+        Debug.Log("Hello");
+        sLogger.LogInfo($"Updating {MyPluginInfo.PLUGIN_GUID}");
+    }
+}
